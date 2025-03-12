a4d4bf3f5f9cd6bc89811fb06abcfa61e6b6b5ec
gregoryDAY <gregory.day@etik.com>
2025-03-12 10:44:16 +0100

    adding user signup validation

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const MONGODB_URI = 'mongodb+srv://udemyShoppingApp:OMQ7Qw6eU8gz1Ubf@cluster0.zmacg.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0'
 
@@ -43,12 +44,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({message: message});
+    const data = error.data;
+    res.status(status).json({message: message, data: data});
 });
 
 mongoose
