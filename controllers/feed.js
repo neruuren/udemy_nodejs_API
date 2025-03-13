d50c241884075722264572d7ced46294b9f95a84
gregoryDAY <gregory.day@etik.com>
2025-03-13 07:35:47 +0100

    adding authorizations check

--- controllers/feed.js
+++ controllers/feed.js
@@ -149,6 +149,11 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if(post.creator.toString() !== req.userId) {
+        const error = new Error('Not authorized !!');
+        error.statusCode = 403;
+        throw error;
+      }
       if(imageUrl !== post.imageUrl) {
         clearImage(post.imageUrl)
       }
@@ -176,6 +181,11 @@ exports.deletePost = (req, res, next) => {
     if(!post) {
       const error = new Error('Could not find Post !!');
       error.statusCode = 404;
+      throw error;
+    }
+    if(post.creator.toString() !== req.userId) {
+      const error = new Error('Not authorized !!');
+      error.statusCode = 403;
       throw error;
     }
     // Check logged in user
